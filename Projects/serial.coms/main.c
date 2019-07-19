2a8e08df91c75dd5798e9f018ec81b942a45ec1c
nickhansenrf <47431195+nickhansenrf@users.noreply.github.com>
2019-07-19 11:36:43 -0300

    initial rs232 coms
    tested with an arduino, I can get it to respond.

---
+++ Projects/serial.coms/main.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "rs232.h"
+
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+
+int main()
+{
+    printf("Serial Communication in C\n");
+    int i, n,
+        cport_nr=5,        /* /dev/ttyS0 (COM1 on windows) */
+        bdrate=9600;       /* 9600 baud */
+
+    unsigned char buf[4096];
+
+    char mode[]= {'8','N','1', 0};
+
+
+    if(RS232_OpenComport(cport_nr, bdrate, mode, 0))
+    {
+        printf("Can not open comport\n");
+
+        return(0);
+    }
+    printf("c to continue, other to quit\n");
+    while(getc(stdin) == 'c')
+    {
+        fflush(stdin);
+
+        if(RS232_SendByte(cport_nr, '1')){
+            printf("error sending byte");
+            return 0;
+        }
+
+        n = RS232_PollComport(cport_nr, buf, 4095);
+
+        if(n > 0)
+        {
+            buf[n] = 0;   /* always put a "null" at the end of a string! */
+
+            for(i=0; i < n; i++)
+            {
+                if(buf[i] < 32)  /* replace unreadable control-codes by dots */
+                {
+                    buf[i] = '.';
+                }
+            }
+
+            printf("received %i bytes: %s\n", n, (char *)buf);
+        }
+
+#ifdef _WIN32
+        Sleep(100);
+#else
+        usleep(100000);  /* sleep for 100 milliSeconds */
+#endif
+    }
+
+    return(0);
+    return 0;
+}
